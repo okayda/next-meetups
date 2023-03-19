21a9329d70042c0e5fabd9dc98355336bbf0ba3a
Jhon <59811681+okayda@users.noreply.github.com>
2023-03-18 20:28:48 -0700

    initial commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = function () {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups creat a amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </>
+  );
+};
+
+export default NewMeetupPage;
